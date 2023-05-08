54b49be44c9c0f0f90aed55d56d1c6e1aa8e4f4a
Adedolapo Adeagbo <dollypalton@Adedolapos-MacBook-Pro.local>
2023-05-08 07:52:18 +0100

    Displaying tours grid with react

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import Loading from "./Components/Loading";
+import Tours from "./Components/Tours";
+
+import { useState, useEffect } from "react";
+
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+  
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+export default App;
---
+++ src/Components/Tours.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="title-underline"></div>
+      </div>
+
+      <div className="tours">
+        {tours.map((tour) => {
+          console.log(tour);
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
